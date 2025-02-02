9ee2233c032c2c0c2dba6e38632ec48339dc97ce
jhozera <152558072+jhozera@users.noreply.github.com>
2025-02-02 20:47:49 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ document.getElementById("button-mobile").onclick = function() {
     let menu = document.getElementById("menu");
 
     if (menu.style.display === "none") {
-        menu.style.display = "block";  // Use assignment operator
+        menu.style.display = "block";  
     } else {
-        menu.style.display = "none";  // Use assignment operator
+        menu.style.display = "none"; 
     }
 }
 
@@ -12,9 +12,9 @@ document.getElementById("button-mobilefechar").onclick = function() {
     let menu = document.getElementById("menu");
 
     if (menu.style.display === "none") {
-        menu.style.display = "block";  // Use assignment operator
+        menu.style.display = "block";  
     } else {
-        menu.style.display = "none";  // Use assignment operator
+        menu.style.display = "none";  
     }
 }
 
